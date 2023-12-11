b536f26fc906bea283289dfee0ce35cf0d8a6aaa
GitHub Action <action@github.com>
2023-12-11 12:48:03 +0000

    Update to 0.3.11

--- Formula/localportal.rb
+++ Formula/localportal.rb
@@ -1,15 +1,15 @@
 class Localportal < Formula
   desc "localportal.io, Normalize serving from localhost"
   homepage "https://localportal.io"
-  version "0.3.10"
+  version "0.3.11"
   depends_on "ttyd"
 
   if Hardware::CPU.intel?
     url "https://storage.localportal.io/cli/#{version}/localportal-darwin-amd64-#{version}.tar.gz"
-    sha256 "3eea9464e7a3daeae9a341d1fcc845ef84c89f6c36c6c7193005be413c803c95"
+    sha256 "db04c8730ca548b4ab98e1fc5b71e8217483c3033c288c430372252d28696d3d"
   elsif Hardware::CPU.arm?
     url "https://storage.localportal.io/cli/#{version}/localportal-darwin-arm64-#{version}.tar.gz"
-    sha256 "fdfc4f076723fc6f7ce259248ace3820f17950bf48bf22fe430b0192a6360ee9"
+    sha256 "48ef81294ee693ef7f6adb9eb60ec2726475f594b9894160f6de4f2aa04d4ecd"
   end
 
   def install
